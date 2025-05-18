81053664026bef6c5d9bbab6a72808e9b01c9a08
overdevop <jaguilar@safeglobal.cl>
2025-05-18 01:51:14 -0400

    Update database connection parameters in conexion.php for correct configuration

--- models/conexion.php
+++ models/conexion.php
@@ -5,9 +5,9 @@ class Conexion
     {
         try {
             $link = new PDO(
-                "mysql:host=localhost;dbname=tiendademascotas",
-                "root",
-                "",
+                "mysql:host=localhost;port=8889;dbname=tiendaDeMascotas",
+                "user",
+                "(4vaz5/SY-BX!BfB",
                 array(
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
